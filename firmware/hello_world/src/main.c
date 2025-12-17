53d3b72fb2d032dd58febf7e03040c2b32769769
yys806 <3492675568@qq.com>
2025-12-17 23:15:31 +0800

    修复make问题

--- firmware/hello_world/src/main.c
+++ firmware/hello_world/src/main.c
@@ -9,12 +9,23 @@
  */
 
 #include <stdint.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <platform.h>
 #include "init.h"
 
 #define STRBUF_SIZE			256	// String buffer size
+
+extern uint64_t get_timer_value(void);
+extern unsigned long get_timer_freq(void);
+
+static void delay_ms(uint32_t ms)
+{
+	uint64_t start = get_timer_value();
+	uint64_t ticks = (get_timer_freq() * (uint64_t)ms) / 1000;
+
+	while ((get_timer_value() - start) < ticks) {
+	}
+}
 
 static uint32_t bcd_encode_4digits(uint32_t value)
 {
@@ -39,7 +50,7 @@ int main(void)
 		MY_PERIPH_REG(MY_PERIPH_REG_IO) = bcd;
 		printf("set display = %04u (bcd=0x%04x)\n", value % 10000, bcd);
 		value++;
-		usleep(1000000);
+		delay_ms(1000);
 	}
 	return 0;
 }
